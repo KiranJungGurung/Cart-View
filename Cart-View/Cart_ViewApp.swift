f7744c88f507f163b6d08957f78d65217da9df69
KiranJungGurung <kirangurung394@gmail.com>
2023-03-27 12:06:05 +0545

    Initial Commit

---
+++ Cart-View/Cart_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cart_ViewApp.swift
+//  Cart-View
+//
+//  Created by Kiran Gurung on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Cart_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
